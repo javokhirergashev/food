8f25a8ba44585db07c3b7ba6748676a3491bf1f8
unknown <iqtidorziyosi@gmail.com>
2024-02-29 19:35:02 +0500

    fix

--- common/models/User.php
+++ common/models/User.php
@@ -21,7 +21,7 @@ use yii\web\IdentityInterface;
  * @property integer $status
  * @property integer $created_at
  * @property integer $updated_at
- * @property string $password write-only password
+ * @property string $password 
  */
 class User extends ActiveRecord implements IdentityInterface
 {
